da78a2b56dbc0634791aaddb0294dbb7087a4030
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-04-04 18:34:34 +0200

    get db connection string from dotenv file

--- db/pg.js
+++ db/pg.js
@@ -1,11 +1,10 @@
-//import "dotenv/config";
+import * as dotenv from 'dotenv';
+dotenv.config()
 import pkg from "pg";
 const { Pool } = pkg;
 
 const connPool = new Pool({
-  // todo: save connection string to ENV variable
-  connectionString:
-    "postgres://jsrjextg:0vU5h7kbfsnldaOLyCr9CsybatpdFe-M@surus.db.elephantsql.com/jsrjextg",
+  connectionString: process.env.DB_CONNECTION
 });
 
 export default connPool;
